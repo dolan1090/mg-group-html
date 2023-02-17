7a92406802ec91f7fbb1b3c0a628ca8903a053c9
vdloc <uvcudlco@gmail.com>
2023-02-17 10:46:56 +0700

    remove jquery and its plugins and refactor header

--- js/main.js
+++ js/main.js
@@ -1,66 +1,94 @@
-// SmartMenus init
-$(function () {
-  $("#main-menu").smartmenus({
-    subMenusSubOffsetX: 6,
-    subMenusSubOffsetY: -8,
+function setupSwiperSlider() {
+  // Setup banner slider
+  var slider = new Swiper('.slide-main', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoHeight: true,
+    autoplay: {
+      delay: 25000,
+      disableOnInteraction: false,
+    },
   });
-});
+}
 
-// SmartMenus mobile menu toggle button
-$(function () {
-  var $mainMenuState = $("#main-menu-state");
-  if ($mainMenuState.length) {
-    // animate mobile menu
-    $mainMenuState.change(function (e) {
-      var $menu = $("#main-menu");
-      if (this.checked) {
-        $menu.hide().slideDown(250, function () {
-          $menu.css("display", "");
-        });
-      } else {
-        $menu.show().slideUp(250, function () {
-          $menu.css("display", "");
-        });
-      }
+// Setup page elements
+function setupElements() {
+  // Setup languages select box
+  const languagesBox = customSelect('#languages-select');
+
+  // Setup scroll to top button
+  const scrollToTopButton = document.querySelector('.back-to-top');
+  scrollToTopButton.addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+}
+
+// Setup mobile smartmenu
+function setupMobileSubMenu() {
+  const mainMenu = document.getElementById('main-menu');
+  const menuStateInput = document.getElementById('main-menu-state');
+  const subMenus = [...mainMenu.querySelectorAll('.sub-menu')];
+  const OPEN_MENU_CLASS = 'highlighted';
+  let maxSubMenuHeight = 0;
+
+  subMenus.forEach((subMenu) => {
+    const menuItemLink = getMenuItemLink(subMenu);
+    const subMenuArrow = document.createElement('span');
+    console.log(subMenus);
+
+    subMenuArrow.classList.add('sub-arrow');
+    subMenuArrow.addEventListener('click', (event) => {
+      event.preventDefault();
+      menuItemLink.classList.toggle(OPEN_MENU_CLASS);
+      maxSubMenuHeight = subMenu.scrollHeight;
+      mainMenu.style.setProperty(
+        '--max-submenu-height',
+        `${maxSubMenuHeight}px`
+      );
     });
-    // hide mobile menu beforeunload
-    $(window).bind("beforeunload unload", function () {
-      if ($mainMenuState[0].checked) {
-        $mainMenuState[0].click();
-      }
-    });
-  }
-});
 
-// Fix bug
-var btn_close_topmenu = document.querySelector("#main-menu-state");
-var main_topmenu = document.querySelector("#main-menu");
-btn_close_topmenu.onclick = function () {
-  if (!main_topmenu.classList.contains("show-mn")) {
-    main_topmenu.classList.add("show-mn");
-  } else {
-    main_topmenu.classList.remove("show-mn");
-  }
-};
+    menuItemLink.classList.add('has-submenu');
+    menuItemLink.append(subMenuArrow);
+  });
 
-// Setup banner slider
-var swiper = new Swiper(".slide-main", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoHeight: true,
-  autoplay: {
-    delay: 25000,
-    disableOnInteraction: false,
-  },
-});
+  menuStateInput.addEventListener('change', () => {
+    if (!menuStateInput.checked) {
+      subMenus.forEach((subMenu) => {
+        const menuItemLink = getMenuItemLink(subMenu);
+        menuItemLink.classList.remove(OPEN_MENU_CLASS);
+      });
+    }
+  });
+}
 
-// Setup languages selectbox customizations
-const languagesSelect = customSelect("#lang_choice_1");
+function getMenuItemLink(subMenu) {
+  const menuItem = subMenu.parentNode;
+  const menuItemChildren = menuItem.childNodes;
+  const subMenuSiblings = [...menuItemChildren].filter(
+    (item) => item !== subMenu && item.nodeName.toLowerCase() === 'a'
+  );
 
-// Scroll to top
-const scrollToTopButton = document.querySelector(".back-to-top");
-scrollToTopButton.addEventListener("click", function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+  if (subMenuSiblings.length === 0) return null;
+
+  return subMenuSiblings[0];
+}
+
+function setupMobileMenu() {
+  // Fix bug
+  var btn_close_topmenu = document.querySelector('#main-menu-state');
+  var main_topmenu = document.querySelector('#main-menu');
+  btn_close_topmenu.onclick = function () {
+    if (!main_topmenu.classList.contains('show-mn')) {
+      main_topmenu.classList.add('show-mn');
+    } else {
+      main_topmenu.classList.remove('show-mn');
+    }
+  };
+}
+
+setupSwiperSlider();
+setupElements();
+setupMobileMenu();
+setupMobileSubMenu();
